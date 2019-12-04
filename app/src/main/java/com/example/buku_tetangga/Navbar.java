ac31c6eb6862f58caec282cdb2164b432490d4d2
AndreDitVir <51712370+AndreDitVir01@users.noreply.github.com>
2019-12-04 20:00:12 +0700

    Beranda

--- app/src/main/java/com/example/buku_tetangga/Navbar.java
+++ app/src/main/java/com/example/buku_tetangga/Navbar.java
@@ -3,12 +3,20 @@ package com.example.buku_tetangga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Navbar extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navbar);
+        setContentView(R.layout.activity_home);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
     }
 }
