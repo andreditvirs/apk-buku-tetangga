02c4dd2fc9dbd77d0f7f2d4e9b78db8aea1e16e5
AndreDitVir <51712370+AndreDitVir01@users.noreply.github.com>
2019-12-04 20:59:37 +0700

    NavBar Fix 4 Fragment

--- app/src/main/java/com/example/buku_tetangga/AkunFragment.java
+++ app/src/main/java/com/example/buku_tetangga/AkunFragment.java
@@ -13,10 +13,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfilFragment extends Fragment {
+public class AkunFragment extends Fragment {
 
 
-    public ProfilFragment() {
+    public AkunFragment() {
         // Required empty public constructor
     }
 
@@ -25,7 +25,7 @@ public class ProfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        return inflater.inflate(R.layout.fragment_akun, container, false);
     }
 
 }
--- app/src/main/java/com/example/buku_tetangga/Navbar.java
+++ app/src/main/java/com/example/buku_tetangga/Navbar.java
@@ -1,8 +1,11 @@
 package com.example.buku_tetangga;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -12,11 +15,51 @@ public class Navbar extends AppCompatActivity {
     private BottomNavigationView mMainNav;
     private FrameLayout mMainFrame;
 
+    private HomeFragment homeFragment;
+    private SewaFragment sewaFragment;
+    private RiwayatFragment riwayatFragment;
+    private AkunFragment akunFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        sewaFragment = new SewaFragment();
+        riwayatFragment = new RiwayatFragment();
+        akunFragment = new AkunFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.nav_beranda:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_sewa:
+                        setFragment(sewaFragment);
+                        return true;
+                    case R.id.nav_riwayat:
+                        setFragment(riwayatFragment);
+                        return true;
+                    case R.id.nav_akun:
+                        setFragment(akunFragment);
+                        return true;
+                    default: return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(androidx.fragment.app.Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
