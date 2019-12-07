0b68afe9bf81ca07fcc8da019d2ed840d9b51535
AndreDitVir <51712370+AndreDitVir01@users.noreply.github.com>
2019-12-07 15:05:59 +0700

    Beranda : Android Image Slider Test 3

---
+++ app/src/main/java/com/example/buku_tetangga/Adapter.java
@@ -0,0 +1,62 @@
+package com.example.buku_tetangga;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class Adapter extends PagerAdapter {
+
+    private List<Model> models;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public Adapter(List<Model> models, Context context) {
+        this.models = models;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return models.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_of_card_home, container, false);
+
+        ImageView imageView;
+        TextView title, desc;
+
+        imageView = view.findViewById(R.id.image_of_card_home);
+        title = view.findViewById(R.id.title_of_card_home);
+        desc = view.findViewById(R.id.desc_of_card_home);
+
+        imageView.setImageResource(models.get(position).getImage());
+        title.setText(models.get(position).getTitle());
+        desc.setText(models.get(position).getDesc());
+
+        container.addView(view, 0);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
--- app/src/main/java/com/example/buku_tetangga/HomeFragment.java
+++ app/src/main/java/com/example/buku_tetangga/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.example.buku_tetangga;
 
 
+import android.animation.ArgbEvaluator;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.transition.Slide;
 import android.view.LayoutInflater;
@@ -15,12 +17,21 @@ import com.smarteist.autoimageslider.IndicatorAnimations;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    // set card_of_home
+    ViewPager viewPager;
+    Adapter adapter2;
+    List<Model> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -47,7 +58,52 @@ public class HomeFragment extends Fragment {
         sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
         sliderView.startAutoCycle();
 
-        // Inflate the layout for this fragment
+        // set card_of_home
+        models = new ArrayList<>();
+        models.add(new Model(R.drawable.brochure, "Brochure", "Ini adalah brosur"));
+        models.add(new Model(R.drawable.sticker, "Stiker", "Ini adalah stiker"));
+        models.add(new Model(R.drawable.poster, "Poster", "Ini adalah poster"));
+        models.add(new Model(R.drawable.namecard, "Namecard", "Ini adalah kartu nama"));
+
+        adapter2 = new Adapter(models, this.getActivity().getApplicationContext());
+
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter2);
+        viewPager.setPadding(130, 0, 130, 0);
+
+        Integer[] colors_temp = {
+                getResources().getColor(R.color.color1),
+                getResources().getColor(R.color.color2),
+                getResources().getColor(R.color.color3),
+                getResources().getColor(R.color.color4)
+        };
+
+        colors = colors_temp;
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if(position < (adapter2.getCount()-1) && position < (colors.length - 1)) {
+                    viewPager.setBackgroundColor(
+                            (Integer) argbEvaluator.evaluate(
+                                    positionOffset, colors[position], colors[position + 1])
+                    );
+                }else{
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
         return rootView;
     }
 
