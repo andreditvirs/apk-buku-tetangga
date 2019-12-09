ae4ecb574b4425d639429f51bd931b57dfdb32a2
AndreDitVir <51712370+AndreDitVir01@users.noreply.github.com>
2019-12-10 04:22:16 +0700

    Foget Password : (Almost Fixed No Tested)

---
+++ app/src/main/java/com/example/buku_tetangga/ForgetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.buku_tetangga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
