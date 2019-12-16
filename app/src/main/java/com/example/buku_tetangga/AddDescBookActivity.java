8e6f98f8c5f4ebdf9ad86f8500dca0de812fe21d
AndreDitVir <51712370+AndreDitVir01@users.noreply.github.com>
2019-12-16 17:44:25 +0700

    Tambah Deskripsi Buku (No Tested Finished)

--- app/src/main/java/com/example/buku_tetangga/AddDescBookActivity.java
+++ app/src/main/java/com/example/buku_tetangga/AddDescBookActivity.java
@@ -2,7 +2,11 @@ package com.example.buku_tetangga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class AddDescBookActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class AddDescBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_desc_book);
+    }
+
+    public void toAddBook(View view) {
+        startActivity(new Intent(this, AddBookActivity.class));
+        Animatoo.animateSlideLeft(this);
     }
 }
